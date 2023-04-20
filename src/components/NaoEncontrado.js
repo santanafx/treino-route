79001eceefd3f381332446bc0a70deb06d54a071
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-04-20 09:25:31 -0300

    Programa simples route react

---
+++ src/components/NaoEncontrado.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NaoEncontrado = () => {
+    return (
+        <div>NaoEncontrado</div>
+    )
+}
---
+++ src/components/Sobre.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Sobre = () => {
+    return (
+        <div>Este é o sobre</div>
+    )
+}
